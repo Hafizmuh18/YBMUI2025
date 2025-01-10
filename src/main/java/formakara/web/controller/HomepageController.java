749516fe5e4ae950af460090e8be8a70911ac4fa
Hafizmuh18 <hafizmuh18@gmail.com>
2025-01-10 22:27:24 +0700

    update homepae&roadshow

--- src/main/java/formakara/web/controller/HomepageController.java
+++ src/main/java/formakara/web/controller/HomepageController.java
@@ -61,7 +61,7 @@ public class HomepageController {
         jadwal5.put("tanggal", "Rabu, 8 Januari 2025");
         jadwal5.put("jam", "08.00-08.30");
         jadwal5.put("warna", "hijau");
-        jadwal5.put("image", "file/comming_soon.png");
+        jadwal5.put("image", "file/imma.JPG");
         listEvent.add(jadwal5);
 
         HashMap<String, String> jadwal6 = new HashMap<>();
@@ -70,7 +70,7 @@ public class HomepageController {
         jadwal6.put("tanggal", "Rabu, 8 Januari 2025");
         jadwal6.put("jam", "11.00-11.45");
         jadwal6.put("warna", "hijau");
-        jadwal6.put("image", "file/comming_soon.png");
+        jadwal6.put("image", "file/tetra.JPG");
         listEvent.add(jadwal6);
 
         HashMap<String, String> jadwal7 = new HashMap<>();
@@ -79,7 +79,7 @@ public class HomepageController {
         jadwal7.put("tanggal", "Rabu, 8 Januari 2025");
         jadwal7.put("jam", "14.00");
         jadwal7.put("warna", "hijau");
-        jadwal7.put("image", "file/comming_soon.png");
+        jadwal7.put("image", "file/man1.jpg");
         listEvent.add(jadwal7);
 
         HashMap<String, String> jadwal8 = new HashMap<>();
@@ -126,6 +126,51 @@ public class HomepageController {
         jadwal12.put("warna", "kuning");
         jadwal12.put("image", "file/comming_soon.png");
         listEvent.add(jadwal12);
+
+        HashMap<String, String> jadwal13 = new HashMap<>();
+        jadwal13.put("id", "12");
+        jadwal13.put("sekolah", "SMA 1 Mempawah");
+        jadwal13.put("tanggal", "Selasa, 14 Januari 2025");
+        jadwal13.put("jam", "07.00");
+        jadwal13.put("warna", "kuning");
+        jadwal13.put("image", "file/comming_soon.png");
+        listEvent.add(jadwal13);
+
+        HashMap<String, String> jadwal14 = new HashMap<>();
+        jadwal14.put("id", "13");
+        jadwal14.put("sekolah", "MAN IC SAMBAS");
+        jadwal14.put("tanggal", "Kamis, 16 Januari 2025");
+        jadwal14.put("jam", "13.00");
+        jadwal14.put("warna", "kuning");
+        jadwal14.put("image", "file/comming_soon.png");
+        listEvent.add(jadwal14);
+
+        HashMap<String, String> jadwal15 = new HashMap<>();
+        jadwal15.put("id", "14");
+        jadwal15.put("sekolah", "SMA 1 SAMBAS");
+        jadwal15.put("tanggal", "Sabtu, 18 Januari 2025");
+        jadwal15.put("jam", "09.00");
+        jadwal15.put("warna", "kuning");
+        jadwal15.put("image", "file/comming_soon.png");
+        listEvent.add(jadwal15);
+
+        HashMap<String, String> jadwal16 = new HashMap<>();
+        jadwal16.put("id", "15");
+        jadwal16.put("sekolah", "SMAN 2 Pontianak");
+        jadwal16.put("tanggal", "Senin, 20 Januari 2025");
+        jadwal16.put("jam", "08.30-10.00");
+        jadwal16.put("warna", "kuning");
+        jadwal16.put("image", "file/comming_soon.png");
+        listEvent.add(jadwal16);
+
+        HashMap<String, String> jadwal17 = new HashMap<>();
+        jadwal17.put("id", "15");
+        jadwal17.put("sekolah", "EXPO SMAN 10 Pontianak");
+        jadwal17.put("tanggal", "Rabu, 22 Januari 2025");
+        jadwal17.put("jam", "08.30");
+        jadwal17.put("warna", "kuning");
+        jadwal17.put("image", "file/comming_soon.png");
+        listEvent.add(jadwal17);
 
         model.addAttribute("listEvent", listEvent);
 
